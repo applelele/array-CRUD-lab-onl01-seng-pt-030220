f59e64949dc6389d0f751e3b867da9fb50f386a9
YUKIKO IKEDA <applelele@gmail.com>
2020-03-06 23:11:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  animals = ["cat","dog"]
-  animals << "giraffe"
+  animals = ["rabbit", "elephant"]
+  animals << ["cat","dog"]
 end
 
 def add_element_to_start_of_array(array, element)
